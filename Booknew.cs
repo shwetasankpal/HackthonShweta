ec356dedd820747e8ce55342850cb102b4730f04
shwetasankpal <shwetassankpal2612@gmail.com>
2025-08-04 12:21:59 +0530

    Add files via upload

---
+++ Booknew.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon_Shweta
+{
+    internal class Booknew
+    {
+        static void Main(string[] args)
+        {
+            List<string> books = new List<string>
+            {
+               "\"The Canterbury Tales\" by Chaucer",
+                "\"Algorithms\" by Sedgewick",
+                "\"The C Programming Language\" by Kernighan and Ritchie"
+
+            };
+
+            List<string> sortTitles = SortTitles(books);
+            foreach (var title in sortTitles)
+            {
+                Console.WriteLine(title);
+            }
+        }
+
+        public static List<string> SortTitles(List<string> bookEntry)
+        {
+            var bookList = new List<(string Author, string Title)>();
+            foreach (var entry in bookEntry)
+            {
+                var parts = entry.Split(new[] { " by " }, StringSplitOptions.None);
+                if (parts.Length != 2)
+                {
+                    continue;
+                }
+                string title = parts[0].Trim('\"', ' ');
+                string author = parts[1].Trim();
+                string fAuthor = author.Split(new[] { "and" }, StringSplitOptions.None)[0];
+                bookList.Add((fAuthor.Trim(), title));
+
+            }
+            var sortedBooks = bookList.OrderBy(b => b.Author).ThenBy(b => b.Title).Select(b => b.Title).ToList();
+            return sortedBooks;
+        }
+    }
+}
