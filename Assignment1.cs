1a09905a4110565323007ccb67787c8c7944e573
shwetasankpal <shwetassankpal2612@gmail.com>
2025-08-04 16:16:30 +0530

    Add files via upload

---
+++ Assignment1.cs
@@ -0,0 +1,21 @@
+﻿namespace ShwetaAssignment
+{
+    internal class Assignment1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Integral Types Min Range are:");
+            Console.WriteLine($"int : {int.MinValue} to {int.MaxValue}");
+            Console.WriteLine($"byte : {byte.MinValue} to {byte.MaxValue}");
+            Console.WriteLine($"short : {short.MinValue} to {short.MaxValue}");
+            Console.WriteLine($"long : {long.MinValue} to {long.MaxValue}\n");
+
+            Console.WriteLine("Floating Types Min Range are:");
+            Console.WriteLine($"float : {float.MinValue} to {float.MaxValue}");
+            Console.WriteLine($"double : {double.MinValue} to {double.MaxValue}");
+            Console.WriteLine($"double : {decimal.MinValue} to {decimal.MaxValue}");
+
+            
+        }
+    }
+}
---
+++ Assignment4.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShwetaAssignment
+{
+    internal class Assignment4
+    {
+       
+      static void Main(String[] args)
+        {
+            Console.WriteLine("choose the data type of array elements");
+            string type = Console.ReadLine().ToLower();
+
+            Console.WriteLine("Enter the size of array");
+            int size = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Enter {size} values of type: {type}");
+
+            if (type == "int")
+            {
+                int[] arr = new int[size];
+                for (int i = 0; i < size; i++)
+                {
+                    Console.WriteLine($"element {i + 1}");
+                    arr[i] = int.Parse(Console.ReadLine());
+
+                }
+                Console.WriteLine("array contents");
+                foreach (int val in arr)
+                {
+                    Console.WriteLine(val + " ");
+                }
+            }
+            else if (type == "double")
+            {
+                double[] arr = new double[size];
+                for (int i = 0; i < size; i++)
+                {
+                    Console.WriteLine($"elements {i + 1}");
+                    arr[i] = double.Parse(Console.ReadLine());
+                }
+                Console.WriteLine("array contents");
+                foreach (double val in arr)
+                {
+                    Console.WriteLine(val + "");
+                }
+
+            }
+            else if (type == "string")
+            {
+                string[] arr = new string[size];
+                for (int i = 0; i < size; i++)
+                {
+                    Console.WriteLine($"elements {i + 1}");
+                    arr[i] = Console.ReadLine();
+                }
+                Console.WriteLine("array contents");
+                foreach (string val in arr)
+                {
+                    Console.WriteLine(val + "");
+                }
+
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+            Console.WriteLine("completed");
+
+        }
+    }
+}
+
+
