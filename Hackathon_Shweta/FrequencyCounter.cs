8fe43dd43da7ad238dc192c5d2e42a8adb116e48
shwetasankpal <shwetassankpal2612@gmail.com>
2025-08-08 11:30:07 +0530

    Add files via upload

---
+++ Hackathon_Shweta/FrequencyCounter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+namespace Hackathon_Shweta
+{
+    internal class FrequencyCounter
+    {
+        static void Main()
+
+        {
+            Console.WriteLine("Enter the sentence");
+            string input = Console.ReadLine();
+            if (!Regex.IsMatch(input, "[a-zA-Z]"))
+            {
+                Console.WriteLine("0");
+                return;
+            }
+            string[] words = input.ToLower().Split(new char[] { ' ', '.', ';', '?', '!', ':', '-', '_', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> freqDict = new Dictionary<string, int>();
+            Console.WriteLine(freqDict);
+            foreach (string word in words)
+            {
+                if (freqDict.ContainsKey(word))
+                {
+                    freqDict[word]++;
+                }
+                else
+                {
+                    freqDict[word] = 1;
+                }
+            }
+            var sortedwords = freqDict.OrderByDescending(x => x.Value).ThenByDescending(x => x.Key);
+
+            foreach (var item in sortedwords)
+            {
+                Console.WriteLine($"{item.Value} {item.Key}");
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
